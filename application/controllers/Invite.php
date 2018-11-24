ae18b68ed457e1236875734bbacc68f2a98191c9
zhang.jiang <zhang.jiang@qianka.com>
2018-11-24 17:31:58 +0800

    add car info model

--- application/controllers/Invite.php
+++ application/controllers/Invite.php
@@ -18,12 +18,34 @@ class Invite extends Base {
     }
 
     /**
-     * 首页
+     * 被邀请人首页
      */
     public function index()
     {
+        if ($this->_data['browser'] == 1) {
+            $this->isLogin();
+        }
         $info = $this->input->request(null, true);
         $data['invite_code'] = $info['invite_code'];
+        if (is_ajax_post()) {
+            $data['code'] = $info['code'];
+            $data['phone'] = $info['phone'];
+            $data['name'] = $info['name'];
+            $data['master_uid'] = $info['invite_code'];
+            $data['car_id'] = $info['car_id'];
+            $data['create_dt'] = NOW_DATE_TIME;
+            $this->Users->addUserInfo($data);
+            $this->AjaxReturn('200','成功',site_url('User', 'center'));
+        }
         $this->displayMain($data);
+
+    }
+
+    /**
+     * 被邀请人信息
+     */
+    public function info()
+    {
+        $this->displayMain();
     }
 }
