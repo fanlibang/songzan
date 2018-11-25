a5c72cebc6ce727f017f9cda080a9cd264fc23ad
zhang.jiang <zhang.jiang@qianka.com>
2018-11-25 16:08:31 +0800

    fix

--- application/views/invite/share.php
+++ application/views/invite/share.php
@@ -1,13 +1,13 @@
 <div class="wrapper">
     <div class="logo"><img src="<?= STATIC_ASSETS ?>images/logo.png" alt=""></div>
     <div class="content bg-2 flex center jc">
-        <div style="background-image: <?=$img_url?>;">
+        <div style="background-image: <?=$img_url?>;pointer-events:none;">
             <div class="gift-tit ta-c">
-                <div><img style="pointer-events:none;" src="<?= STATIC_ASSETS ?>images/gift-tit.png" alt=""></div>
+                <div><img src="<?= STATIC_ASSETS ?>images/gift-tit.png" alt=""></div>
                 <div class="ta-c ma"><?=$invite_code?></div>
             </div>
             <div class="code">
-                <div class="auto"><img style="pointer-events:none;" width="144px" height="144px" src="<?= $qr_code_img ?>" alt=""></div>
+                <div class="auto"><img width="144px" height="144px" src="<?= $qr_code_img ?>" alt=""></div>
             </div>
         </div>
     </div>
