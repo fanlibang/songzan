95a4c2ea5ab2f82dcaa59cf2fc102be9d3bbcce7
xj413766416 <413766416@qq.com>
2018-11-27 20:12:18 +0800

    1

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -1,43 +1,45 @@
 <script src="<?= STATIC_ASSETS ?>js/sendSMS.js" type="text/javascript"></script>
+
+
 <div class="wrapper">
     <div class="logo"><img src="<?= STATIC_ASSETS ?>images/logo.png" alt=""></div>
     <div class="content bg-1">
-        <div class="index-tit ta-c">推荐好友购买路虎.揽胜、揽胜运动版<br/>
-            您和您的好友均可尊享万元推荐礼包</div>
-        <div class="index-btn auto"><a href="<?php echo site_url('User', 'referee'); ?>"><img src="<?= STATIC_ASSETS ?>images/btn-1.png" alt=""></a> </div>
-        <div class="index-btn auto progress"><a href="javascript:;"><img src="<?= STATIC_ASSETS ?>images/btn-2.png" alt=""></a> </div>
-        <div class="rule-box">
-            <a href="javascript:;">
-                <div class="rule auto ta-c">查看活动规则</div>
+        <div class="index-tit ta-c">
+            成功推荐好友购买路虎揽胜、路虎揽胜运动版<br>
+            您和您的好友均可赢取至臻礼包
+        </div>
+        <div class="index-btn auto"><a href="<?php echo site_url('User', 'referee'); ?>" onclick="cc('index/tyg')" ><img src="<?= STATIC_ASSETS ?>images/btn-1.png" alt=""></a> </div>
+        <div class="index-btn auto progress"><a href="javascript:;" onclick="cc('index/tyg')"><img src="<?= STATIC_ASSETS ?>images/btn-2.png" alt=""></a> </div>
+        <div class="rule-box ta-c">
+            <a href="javascript:;" class="rule gz">
+                查看活动规则
             </a>
         </div>
     </div>
 </div>
 <!--弹框-->
-<div class="bomb-wrapper flex jc hide">
-    <div>
-        <div class="bomb-content">
-            <div class="pop-tit ta-c">验 证 登 录</div>
-            <div class="form auto">
-                <div class="form-list flex center">
-                    <label>手机号：</label>
-                    <div class="form-box">
-                        <input type="tel" name="phone" id="phone" value="" class="input-text">
-                    </div>
-                </div>
-                <div class="form-list flex center">
-                    <label>验证码：</label>
-                    <div class="form-box">
-                        <input type="tel" id="verify" value="" class="input-text">
-                    </div>
-                    <input type="button" value="发送验证码" id="code" name="code" class="sendbtn _sms_verify">
-                </div>
-                <div class="form-push">
-                    <input type="button" value="提     交" class="btn auto" id="sub">
+<div class="bomb-wrapper flex center jc hide" id="login">
+    <div class="bomb-content">
+        <div class="pop-tit ta-c">验 证 登 录</div>
+        <div class="form auto">
+            <div class="form-list flex center">
+                <label>手机号：</label>
+                <div class="form-box">
+                    <input type="tel" class="input-text">
                 </div>
             </div>
-            <div class="close"><img src="<?= STATIC_ASSETS ?>images/icon-4.png" alt=""></div>
+            <div class="form-list flex center">
+                <label>验证码：</label>
+                <div class="form-box">
+                    <input type="tel" class="input-text">
+                </div>
+                <input type="button" value="发送验证码" class="sendbtn">
+            </div>
+            <div class="form-push">
+                <input type="button" value="提     交" class="btn auto" onclick="cc('index/tyg')" >
+            </div>
         </div>
+        <div class="close"><img src="<?= STATIC_ASSETS ?>images/icon-4.png" alt=""></div>
     </div>
 </div>
 <div class="bomb-wrapper flex center jc hide" id="hint">
@@ -50,7 +52,7 @@
                 <input type="button" value="我 要 推 荐" class="btn auto">
             </div>
         </div>
-        <div class="close"><img src="images/icon-4.png" alt=""></div>
+        <div class="close"><img src="<?= STATIC_ASSETS ?>images/icon-4.png" alt=""></div>
     </div>
 </div>
 <div class="bomb-wrapper flex center jc hide" id="rule">
@@ -69,7 +71,7 @@
                 <input type="button" value="我 已 阅 读" class="btn auto" id="agree">
             </div>
         </div>
-        <div class="close"><img src="images/icon-4.png" alt=""></div>
+        <div class="close"><img src="<?= STATIC_ASSETS ?>images/icon-4.png" alt=""></div>
     </div>
 </div>
 <script type="text/javascript">
