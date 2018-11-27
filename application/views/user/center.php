263e9cd0ac43ecd8e70ab90271ebcafa3699da50
zhang.jiang <zhang.jiang@qianka.com>
2018-11-27 22:26:39 +0800

    test

--- application/views/user/center.php
+++ application/views/user/center.php
@@ -53,3 +53,9 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+        // 通过下面这个API隐藏右上角按钮
+        WeixinJSBridge.call('hideOptionMenu');
+    });
+</script>
