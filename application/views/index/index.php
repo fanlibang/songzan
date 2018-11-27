aca75d9ae88548cb05a718f1b8983e9e275b65bf
xuj <xuj@kingnet.com>
2018-11-27 09:43:55 +0800

    1

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -53,7 +53,7 @@
             }
             $.ajax({
                 type:'post',
-                url:'<?php echo site_url('Publics', 'login'); ?>',
+                url:'<?php echo site_url('User', 'login'); ?>',
                 data:{phone: phone, code:code},
                 dataType:'json',
                 success:function(json){
