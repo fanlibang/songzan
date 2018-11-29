2bfa22a6fa1b9802256117e5b8c747987f7eeb98
xuj <xuj@kingnet.com>
2018-11-29 12:17:20 +0800

    1

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -218,7 +218,7 @@ class Base extends Common
         //echo $url;exit;
         $redirect_uri = urlencode($url);
         //对url处理，此url为访问上面jump方法的url
-        echo $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=$state#wechat_redirect&invite_code=$invite_code";exit;
+        echo $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=$state&invite_code=$invite_code#wechat_redirect";exit;
         header('Location:' . $url);
     }
 
