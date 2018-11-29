bd6da73d68d694a5ba48c2b023209ee7b02b5ee0
xuj <xuj@kingnet.com>
2018-11-29 23:03:58 +0800

    1

--- application/views/invite/editInfo.php
+++ application/views/invite/editInfo.php
@@ -36,7 +36,7 @@
                 </div>
                 <div class="flex justify">
                     <div class="form-checkbox active">
-                        我已同意保密条款和<a href="javascript:;" class="item">隐私政策</a>
+                        我已同意保密条款和<a href="https://www.landrover.com.cn/cookie-and-privacy-policy.html" class="item">隐私政策</a>
                     </div>
                     <div class="form-tip">标*为必填</div>
                 </div>
