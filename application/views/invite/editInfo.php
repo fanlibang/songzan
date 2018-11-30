b4c6506710b62a4529d3bb9437dad49b0796be2f
xuj <xuj@kingnet.com>
2018-11-30 17:47:36 +0800

    1

--- application/views/invite/editInfo.php
+++ application/views/invite/editInfo.php
@@ -71,8 +71,8 @@
     $(function(){
         $('#car_id').change(function() {
             var car_id = $("#car_id option:selected").val();
-            if (car_id != 1 & car_id != 2) {
-                $('#title').html('参加本次活动的车型为揽胜或揽胜运动版');
+            if (car_id != 1 && car_id != 2 && car_id != 0) {
+                $('#title').html('感谢您的选择，此次活动需在成功购买路虎揽胜、路虎揽胜运动版后，方可赢取至臻礼包。');
                 $('#agree').val('确认');
                 $('#hint').removeClass('hide');
                 //alert('参加本次活动的车型为揽胜或揽胜运动版');
