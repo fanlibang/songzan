82d3a845044cb681af513671de840de9ffc2d3f8
xuj <xuj@kingnet.com>
2018-11-30 11:52:43 +0800

    1

--- application/views/invite/index.php
+++ application/views/invite/index.php
@@ -114,7 +114,7 @@
     $(function(){
         $('#car_id').change(function() {
             var car_id = $("#car_id option:selected").val();
-            if (car_id != 1 || car_id != 2) {
+            if (car_id != 1 && car_id != 2) {
                 $('#title').html('参加本次活动的车型为揽胜或揽胜运动版');
                 $('.tj').val('确认');
                 $('#hint').removeClass('hide');
