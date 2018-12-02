4f12cf4888cbabfc2cf1926961b60de7aafedc04
xj413766416 <413766416@qq.com>
2018-12-02 17:25:11 +0800

    1

--- application/views/admin/member/index.php
+++ application/views/admin/member/index.php
@@ -34,7 +34,7 @@
             <!--<li><a class="delete" href="<?php echo site_url($controller, 'del', array('id' => '{sid}')); ?>" target="ajaxTodo" title="确定要删除吗?"><span>删除</span></a></li>-->
         </ul>
     </div>
-    <table class="table" width="100%" layoutH="110">
+    <table class="list" width="100%" layoutH="110">
         <thead>
         <tr>
             <th>ID</th>
@@ -66,7 +66,7 @@
                     <td><?php echo $v['driver_number']; ?></td>
                     <td><?php echo $v['card_number']; ?></td>
                     <td><?php echo $v['invite_code']; ?></td>
-                    <td><img src="<?php echo $v['qr_code_img']; ?>" width="117px" height="117px"></td>
+                    <td><img src="<?php echo $v['qr_code_img']; ?>" width="117" height="117"></td>
                     <td><?php echo $v['source_name']; ?></td>
                     <td><?php echo $v['created_at']; ?></td>
                     <td>
