bb665c28ab8a7bf73189fa426070697b125ceb46
xj413766416 <413766416@qq.com>
2018-12-02 14:20:48 +0800

    1

--- application/views/user/referee.php
+++ application/views/user/referee.php
@@ -153,6 +153,14 @@ $(document).ready(function(){
 });
     $(function(){
         $('#sub').click(function(){
+            window.alert = function(name){
+                var iframe = document.createElement("IFRAME");
+                iframe.style.display="none";
+                iframe.setAttribute("src", 'data:text/plain,');
+                document.documentElement.appendChild(iframe);
+                window.frames[0].window.alert(name);
+                iframe.parentNode.removeChild(iframe);
+            };
             var name = $('#name').val();
             var phone = $('#phone').val();
             var code = $('#verify').val();
