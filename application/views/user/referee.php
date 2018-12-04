5572a377d5ba0881f28ac9f8b6eddeaf790d6a9f
xuj <xuj@kingnet.com>
2018-12-04 11:36:50 +0800

    1

--- application/views/user/referee.php
+++ application/views/user/referee.php
@@ -79,7 +79,7 @@
     <div class="bomb-content">
         <div class="hint auto">
             <div class="hint-word" id="title">
-                活动礼遇将根据您所提交的信息进行审核派发。确认提交前，请确保信息的准确性。
+                活动礼遇将在信息审核通过后进行寄送。确认提交前，请确保信息的准确性。
             </div>
             <div class="form-push">
                 <input type="button" value="我 要 推 荐" class="btn auto " id="tj" >
@@ -93,7 +93,7 @@
     <div class="bomb-content">
         <div class="hint auto">
             <div class="hint-word">
-                活动礼遇将根据您所提交的信息进行审核派发。确认提交前，请确保信息的准确性。
+                活动礼遇将在信息审核通过后进行寄送。确认提交前，请确保信息的准确性。
             </div>
             <div class="form-push">
                 <input type="button" value="确 认 提 交" class="btn auto " id="from_sub" >
