7012e193525e12bec0300ab30f68408671cbff8c
xuj <xuj@kingnet.com>
2018-12-06 23:55:56 +0800

    后台地址修改

--- application/views/publics/footer.php
+++ application/views/publics/footer.php
@@ -1,7 +1,7 @@
 </body>
 <script type="text/javascript">
     function cc(str){
-        var url = "<?php echo site_url('Publics', 'button') ?>" + '?url='+str;
+        var url = "<?php echo site_url('Publics', 'button') ?>" + '&url='+str;
         $.get(url, function(result){
             console.log(result);
         });
