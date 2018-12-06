43e07d18619ea715ef50e94914e23fc24caa86fc
xuj <xuj@kingnet.com>
2018-12-06 15:23:00 +0800

    后台来源修改

--- application/helpers/function_helper.php
+++ application/helpers/function_helper.php
@@ -910,7 +910,7 @@ if (! function_exists('path_site_url')) {
         foreach ((array)$params as $k => $v) {
             $url_str .= ($url_str ? '&' : '?').$k.'='.$v;
         }
-        
+
 	    return 'http://'.$_SERVER['HTTP_HOST'] . '/2018/crm/ownerreferral/index.php/'.PROJECT_NAME.$controller.'/'.$action.$url_str;
         //return 'http://'.$_SERVER['HTTP_HOST'] . '/'.PROJECT_NAME.'/'.$controller.'/'.$action.$url_str;
     }
@@ -1882,16 +1882,5 @@ function getSinaShortUrl($source, $url_long){
 
     return $result[0]['url_short'];
 
-}
-
-function is_https() {
-    if ( !empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off') {
-        return true;
-    } elseif ( isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' ) {
-        return true;
-    } elseif ( !empty($_SERVER['HTTP_FRONT_END_HTTPS']) && strtolower($_SERVER['HTTP_FRONT_END_HTTPS']) !== 'off') {
-        return true;
-    }
-    return false;
 }
 
