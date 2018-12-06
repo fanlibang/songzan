9a356acddcca499d68db1e062f2888189aa0b9a0
xuj <xuj@kingnet.com>
2018-12-06 10:07:28 +0800

    添加短信功能

--- application/views/publics/header.php
+++ application/views/publics/header.php
@@ -7,10 +7,8 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
     <meta name="format-detection" content="telephone=no, email=no"/>
     <meta name="msapplication-tap-highlight" content="no">
-    <!--
-    <meta content="https://www.landrover.com.cn/resources/public/images/logo-144x144.png" name="msapplication-TileImage">
-    <link href="https://www.landrover.com.cn/resources/public/images/favicon-landrover-152x152.png" rel="apple-touch-icon" sizes="152x152">
-    -->
+    <meta content="<?= STATIC_ASSETS ?>images/logo.png" name="msapplication-TileImage">
+    <link href="<?= STATIC_ASSETS ?>images/logo.png" rel="apple-touch-icon" sizes="152x152">
     <title>荐入佳境 共揽胜景</title>
     <link href="<?= STATIC_ASSETS ?>css/base.css?t=<?=time()?>" rel="stylesheet" type="text/css">
     <script src="<?= STATIC_ASSETS ?>js/jquery.min.js" type="text/javascript"></script>
