daf86f6cad384b27fec75cc3f970796553317e0d
xuj <xuj@kingnet.com>
2018-12-06 20:21:22 +0800

    后台来源修改

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'index.php';
+$route['default_controller'] = '/index.php';
 $route['404_override'] = 'Index/index';
 $route['translate_uri_dashes'] = FALSE;
