59f318b10623f1587d3a309a5fb378f1f694746d
xuj <xuj@kingnet.com>
2018-12-25 16:07:51 +0800

    1

--- application/controllers/Invite.php
+++ application/controllers/Invite.php
@@ -126,7 +126,7 @@ class Invite extends Base
                     'nameplate_of_interest' => $carInfo['alias'],
                     'creation_time'         => NOW_DATE_TIME,
                     'need_lms'              => 1,
-                    'other1'                => $masterUserInfo['phone'],
+                    'other1'                => $masterUserInfo['phone'].'+'.$data['name'],
                 ];
                 $push = new ReportModel();
                 $result = $push->reportOwner($tempData);
