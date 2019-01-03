ecdaeb6a0a366e414fa917d08dd21c68327c8414
xuj <xuj@kingnet.com>
2019-01-03 13:25:48 +0800

    1

--- application/controllers/admin/Audit.php
+++ application/controllers/admin/Audit.php
@@ -117,6 +117,20 @@ class Audit extends Base
             $res = $this->Car->editUserCar($id, ['state' => $state]);
             if ($res) {
                 $this->User->editUserId($uid, ['status' => $state]);
+                if($state == 3) {
+                    //生成短连接
+                    $invite_info    = $this->User->getUserInfoByid($uid);
+                    $user_info      = $this->User->getUserInfoByid($invite_info['master_uid']);
+                    //$long_url = site_url('User', 'center');
+                    $long_url = 'http://'.$_SERVER['HTTP_HOST'] . '/2018/crm/ownerreferral/index.php?c=Invite&m=index';
+                    $short_url = getSinaShortUrl('1555751977',$long_url);
+                    $sms_notice_obj = new SendSms();
+                    $mgs[0] = $user_info['name'];
+                    $mgs[1] = $short_url;
+                    $ret = $sms_notice_obj->send($invite_info['phone'], $mgs, 4);
+                    $update['content'] = json_encode($ret);
+                    $this->Car->editUserCar($uid, $update);
+                }
                 $this->dwzAjaxReturn(200, '操作成功');
             } else {
                 $this->dwzAjaxReturn(201, '操作失败');
